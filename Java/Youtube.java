f580e9117b6214847b6c9cc8ee7b0bafa0e97b34
mampvenom <mampvenom@gmail.com>
2016-07-07 18:47:39 +0900

    Update Youtube.java

--- Java/Youtube.java
+++ Java/Youtube.java
@@ -1,6 +1,18 @@
 public class Youtube {
   
   private String urlToYoutubeID(String url) {
+        String strPattern = "\\W([\\w-]{9,})(\\W|$)";
+        Pattern pattern = Pattern.compile(strPattern);
+
+        Matcher matcher = pattern.matcher(url);
+        if (matcher.find()){
+            System.out.println(matcher.group(1));
+            return matcher.group(1);
+        }
+        return "";
+    }
+  
+  private String urlToYoutubeID(String url) {
     String strPattern = "\\W([\\w-]{9,})(\\W|$)";
     Pattern pattern = Pattern.compile(strPattern);
 
